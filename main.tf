5c306e65c9282d0cd0fa41cc59b72fc5edb2259b
mirceaconstantin434 <mirceaconstantin434@outlook.com>
2025-07-22 23:51:29 +0300

    Correct vulnerability

--- main.tf
+++ main.tf
@@ -30,6 +30,8 @@ resource "azurerm_storage_account" "sa" {
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+
+  min_tls_version          = "TLS1_2"  # ✅ Fix: secure TLS version
   tags                     = local.tags
 }
 
