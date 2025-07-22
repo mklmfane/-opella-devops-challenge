0b1c1c55bad57c440518837b15b9dd4344d4a0fa
mirceaconstantin434 <mirceaconstantin434@outlook.com>
2025-07-23 00:05:50 +0300

    Correct vulnerability

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "dev-rg-terraform"
-    storage_account_name = "mytfstateaccount"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
